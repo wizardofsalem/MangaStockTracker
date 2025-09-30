359d6fd29645f03605b42682d042d834fef24cbc
Cal Lees <caltriestocode@gmail.com>
2025-09-30 22:24:26 +0100

    Initial todo

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "log"
-
 func main() {
-	log.Println("Hello")
+
+	//read in bought.csv
+	//read in sold.csv
+
+	//calculate valueSold-valueBought
+
+	//How to sensibily determine price of current stock?
+	//have to calculate relative time periods of when stock bought and sold
 }
